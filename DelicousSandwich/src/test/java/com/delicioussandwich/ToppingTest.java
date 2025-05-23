d65162d1409465504fbddb0906ae0bd9946b5d50
olumide-AI <olumidekolawole26@gmail.com>
2025-05-23 15:42:58 -0700

    minimal changes to unit test just spelling errors

--- DelicousSandwich/src/test/java/com/delicioussandwich/ToppingTest.java
+++ DelicousSandwich/src/test/java/com/delicioussandwich/ToppingTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ToppingTest {
+    /**
+     * Unit test for all Topping price, regular, premium and extra-premimum
+     */
 
     @Test
     public void testThatRegularToppingWillAlwaysBeZero(){

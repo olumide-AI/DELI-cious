8f2d4d3a48fdba6a79ed244ba6adcfda3bc2722c
olumide-AI <olumidekolawole26@gmail.com>
2025-05-23 16:37:39 -0700

    Added unit test for sandwich prices with different variation

---
+++ DelicousSandwich/src/test/java/com/delicioussandwich/SandwichTest.java
@@ -0,0 +1,58 @@
+package com.delicioussandwich;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for the sandwich class testing the base price and multiple topping calculations scenarios
+ */
+public class SandwichTest {
+    @Test
+    public void basicSandwichTest(){
+        //Arrange
+        Sandwich sandwich = new Sandwich("4", "white", false);
+
+        //Act
+        double sandwichPrice = sandwich.menuItemPrice();
+
+        //Assert
+        assertEquals(5.50, sandwichPrice, 0.001);
+    }
+    @Test
+    public void basicSandwichWithRegularToppingTest(){
+        //Arrange
+        Sandwich sandwich = new Sandwich("4", "white", false);
+        sandwich.addTopping(new RegularTopping("Avocado"));
+
+        //Act
+        double sandwichPrice = sandwich.menuItemPrice();
+
+        //Assert
+        assertEquals(5.50, sandwichPrice, 0.001);
+    }
+    @Test
+    public void basicSandwichWithPremiumToppingTest(){
+        //Arrange
+        Sandwich sandwich = new Sandwich("4", "white", false);
+        sandwich.addTopping(new PremiumTopping("Beef", false));
+
+        //Act
+        double sandwichPrice = sandwich.menuItemPrice();
+
+        //Assert
+        assertEquals(6.50, sandwichPrice, 0.001);
+    }
+
+    @Test
+    public void basicSandwichWithExtraPremiumToppingTest(){
+        //Arrange
+        Sandwich sandwich = new Sandwich("4", "white", false);
+        sandwich.addTopping(new PremiumTopping("Steak", true));
+
+        //Act
+        double sandwichPrice = sandwich.menuItemPrice();
+
+        //Assert
+        assertEquals(7.00, sandwichPrice, 0.001);
+    }
+
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-DELI-cious is a full-featured Java application simulating a deli ordering system. Users can customize sandwiches, add chips and drinks, review order summaries, and generate timestamped receipts. Built with clean OOP principles, the project includes a CLI, a GUI (JavaFX/Swing), full JUnit test coverage, and GitHub Actions for continuous integration.
